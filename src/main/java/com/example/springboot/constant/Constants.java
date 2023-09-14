313f927376240f0e0648d1f0e191c8476202a7a8
PhanCongTu <phancongtu25032002@gmail.com>
2023-09-14 21:52:53 +0700

    Update EMAIL_REGEX

--- src/main/java/com/example/springboot/constant/Constants.java
+++ src/main/java/com/example/springboot/constant/Constants.java
@@ -15,8 +15,7 @@ public final class Constants {
     public static final String NOT_ACCEPTABLE = "NOT_ACCEPTABLE";
     public static final String LOGIN_NAME_REGEX = "^([a-zA-Z0-9._-]{4,16}$)";
     public static final String PASSWORD_REGEX = "^[!-~]{8,20}$";
-    public static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}\\@[a-zA-Z0-9][a-zA-Z0-9\\-]{0,62}(\\.[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})+$";
     private Constants(){
     }
 }
